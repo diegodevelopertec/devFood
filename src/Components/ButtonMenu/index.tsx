0a17db15ce10b2362b5a2470d0ca3f5ed4c7272c
Diego Morais <diegodutramorais@gmail.com>
2022-12-08 03:39:28 -0300

    bug da page

--- src/Components/ButtonMenu/index.tsx
+++ src/Components/ButtonMenu/index.tsx
@@ -9,14 +9,18 @@ type Prop={
     iconActive:boolean;
     onClick?:()=>void;
     link?:string,
-    text?:string
+    text?:string,
+    
 }
 
 
 export const ButtonMenu=({src,bg,marginhorizontal,marginvertical,onClick,iconActive,link,text}:Prop)=>{
-    return <S.ContainerButtonMenu href={link} bgColor={bg} mh={marginhorizontal} mv={marginvertical} bgColorActive={iconActive} onClick={onClick} >
-      <S.ButtonIcon src={src}/>
-      <S.TextButton>
+    return <S.ContainerButtonMenu href={link}  bgColor={bg} mh={marginhorizontal} mv={marginvertical} bgColorActive={iconActive} onClick={onClick} >
+    
+      
+        <S.ButtonIcon src={src} />
+          
+        <S.TextButton>
         {text}
       </S.TextButton>
     </S.ContainerButtonMenu>
